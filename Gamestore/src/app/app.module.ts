5c2a64fa5a2d77440dfcd7339d059c8449f30e89
Simeon Itov <simeon.itov@gmail.com>
2023-07-17 20:04:39 +0300

    Adding Login and Register components. Also routing.

--- Gamestore/src/app/app.module.ts
+++ Gamestore/src/app/app.module.ts
@@ -3,41 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule, Routes, UrlSerializer} from '@angular/router';
+import { RouterModule, Routes, UrlSerializer } from '@angular/router';
 import { CoreModule } from './core/core.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+
+import { HeroComponent } from './core/hero/hero.component';
+import { CatalogComponent } from './user/catalog/catalog.component';
+import { ContactUsComponent } from './core/contact-us/contact-us.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 const appRoutes: Routes = [
-//   { path: '', pathMatch: 'full', component: HeroSectionComponent },
-//   { path: 'catalog', component: CatalogComponent },
-//   { path: 'about-us', component: AboutUsComponent },
-//   { path: 'contact-us', component: ContactUsComponent },
-//   { path: 'login', component: LoginComponent },
-//   { path: 'register', component: RegisterComponent },
-//   { path: ':id/details', pathMatch: 'full', component: DetailsComponent },
-//   { path: 'create', component: CreateComponent, canActivate: [loginGuard] },
-//   {
-//     path: ':id/edit',
-//     pathMatch: 'full',
-//     component: EditComponent,
-//     canActivate: [loginGuard, ownerGuard],
-//   },
-//   { path: 'profile', component: ProfileComponent, canActivate: [loginGuard] },
-//   { path: '**', component: NotFoundComponent },
+  { path: '', pathMatch: 'full', component: HeroComponent },
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  //   { path: ':id/details', pathMatch: 'full', component: DetailsComponent },
+  //   { path: 'create', component: CreateComponent, canActivate: [loginGuard] },
+  //   {
+  //     path: ':id/edit',
+  //     pathMatch: 'full',
+  //     component: EditComponent,
+  //     canActivate: [loginGuard, ownerGuard],
+  //   },
+  //   { path: 'profile', component: ProfileComponent, canActivate: [loginGuard] },
+  //   { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CoreModule,
+    AuthModule,
     UserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- Gamestore/src/app/auth/auth.module.ts
+++ Gamestore/src/app/auth/auth.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CatalogComponent } from './catalog/catalog.component';
-import { DetailsComponent } from './details/details.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { RouterModule } from '@angular/router';
-
 
 
 @NgModule({
   declarations: [
-    CatalogComponent,
-    DetailsComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule, RouterModule
   ],
   exports: [
-    CatalogComponent,
-    DetailsComponent
+    LoginComponent,
+    RegisterComponent
   ]
 })
-export class UserModule { }
+export class AuthModule { }
